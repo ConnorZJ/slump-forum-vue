52a8f290f8823f9ad32b0b90f93b48b1108ff075
ConnorZJ <1020379482@qq.com>
2020-11-10 23:42:47 +0800

    登录

---
+++ slump-vue/src/pages/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-form :model="loginForm" :rules="loginRule" ref="loginForm" label-width="100px">
+    <el-form-item label="账号" prop="username">
+      <el-input v-model="loginForm.username" placeholder="请输入账号"></el-input>
+    </el-form-item>
+    <el-form-item label="密码" prop="password">
+      <el-input v-model="loginForm.password" show-password placeholder="请输入密码"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm('loginForm')">登录</el-button>
+      <el-button @click="toRegist()">注册</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loginForm: {
+        username: '17370012735',
+        password: '123456'
+      },
+      loginRule: {
+        username: [
+          { required: true, message: '请输入账号！', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        password: []
+      }
+    }
+  }
+}
+</script>
+
+<style></style>
